bb12deef6656031d193fb2b8293ef2fa5ea9504b
Peter Jones <pjones@redhat.com>
2018-08-29 15:03:25 -0400

    Lots of minor stuff
    Signed-off-by: Peter Jones <pjones@redhat.com>

--- untty.c
+++ untty.c
@@ -17,11 +17,32 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
-#define debug getenv("UNTTY_DEBUG")
+extern char *__progname;
+
+static bool debug_arg_ = false;
+#define debug_arg ({ if(debug_arg_ || getenv("UNTTY_DEBUG")) debug_arg_ = true; debug_arg_; })
+#define debug(fmt, ...) ({ char *f_ = __FILE__; int l_ = __LINE__;              \
+                if (debug_arg) {                                                \
+                        char *s_ = NULL;                                        \
+                        int rc_;                                                \
+                        fprintf(stderr, "%s: %s:%d ", __progname, f_, l_);      \
+                        rc_ = asprintf(&s_, fmt, ##__VA_ARGS__);                \
+                        for (int i_ = 0; rc_ > 0 && s_ && s_[i_]; i_++) {       \
+                                if (isprint(s_[i_]))                            \
+                                        fprintf(stderr, "%c", s_[i_]);          \
+                                else                                            \
+                                        fprintf(stderr, "\\x%02hhx", s_[i_]);   \
+                        }                                                       \
+                        if (s_)                                                 \
+                                free(s_);                                       \
+                        fprintf(stderr, "\n");                                  \
+                }                                                               \
+        })
 
 #define ESC '\x1b'
 #define SPC '\x20'
 #define CR '\x0d'
+#define NL '\x0a'
 
 void __attribute__((__noreturn__))
 usage(int rc)
@@ -40,27 +61,33 @@ typedef enum states {
 } state_t;
 
 static void
-print_buf(FILE *out, char *buf, ssize_t pos, char escape)
+print_buf(FILE *out, char *buf, ssize_t pos)
 {
-        for (int i = 0; i < pos; i++) {
-                if (buf[i] != escape || isprint(escape)) {
-                        fputc(buf[i], out);
-                        continue;
-                }
-
-                if (isprint(escape))
-                        fputc(escape, out);
-                else if (debug)
-                        fprintf(out, "\\x%02hhx", escape);
+        if (debug_arg) {
+                out = stderr;
+                fprintf(out, "print_buf:\"");
         }
-
-        fflush(out);
+        for (int i = 0; i < pos; i++) {
+                if (!debug_arg && buf[i] == CR)
+                        continue;
+                if (isprint(buf[i]) || !debug_arg) {
+                        fputc(buf[i], out);
+                } else {
+                        fprintf(out, "\\x%02hhx", buf[i]);
+                }
+        }
+        if (debug_arg) {
+                fprintf(out, "\"\n");
+                fflush(out);
+        }
 }
 
 static const char * escapes[] = {
         "[=[[:digit:]]\\+h",
-        "[\\?\\*[[:digit:]]\\+[l]",
-        "[[[:digit:]]\\+;[[:digit:]]\\+[Hmr ]",
+        "[[[:digit:]]\\+[Jlm]",
+        "[\\?[[:digit:]]\\+[Jlm]",
+        "[[[:digit:]]\\+;[[:digit:]]\\+[Hmr]",
+        "\\([[[:digit:]]\\+;[[:digit:]]\\+\\) ", // sometimes console(1) logs this
 #if 0
         "[\\?\\*-\\*[[:digit:]]\\+;-\\*[[:digit:]]\\+[KHJfhlmstu;]",
         "[\\?\\*-\\*[[:digit:]]\\+[KHJfhlmstu;]",
@@ -88,6 +115,22 @@ setup_regexps(regex_t *regexps)
         }
 }
 
+static char *printables(char *str)
+{
+        static char buf[1024];
+        char *current = buf;
+
+        buf[0] = '\0';
+        for (int i = 0; str && str[i]; i++) {
+                if (isprint(str[i]))
+                        current = stpncpy(current, str+i, 1);
+                else
+                        current += sprintf(current, "\\x%02hhx", str[i]);
+                buf[i+1] = '\0';
+        }
+        return buf;
+}
+
 static ssize_t
 match(regex_t *regexps, char *buf, ssize_t pos)
 {
@@ -96,12 +139,13 @@ match(regex_t *regexps, char *buf, ssize_t pos)
         ssize_t ret = -1;
         int matched = -1;
 
+        buf[pos] = '\0';
+
         memset(matches, 0, sizeof(matches));
         for (unsigned int i = 0; escapes[i] != NULL; i++) {
                 int rc;
 
-                if (debug)
-                        fprintf(stderr, "pos:%zd str:\"%s\" regexp:\"%s\"\n", pos, buf+1, escapes[i]);
+                debug("regexec(\"%s\", \"%s\", %zd)", escapes[i], printables(buf+1), pos-1);
                 rc = regexec(&regexps[i], buf+1, pos-1, matches, 0);
                 if (rc != 0 && rc != REG_NOMATCH) {
                         regerror(rc, &regexps[i], errbuf, sizeof(errbuf));
@@ -109,12 +153,12 @@ match(regex_t *regexps, char *buf, ssize_t pos)
                 }
                 if (rc == REG_NOMATCH)
                         continue;
-                if (debug)
-                        printf("found a match: %s\n", escapes[i]);
-                for (int j = 0; matches[j].rm_so != -1; j++)
+                debug("rc:%d\n", rc);
+                debug("found a match: %s", escapes[i]);
+                for (int j = 0; j < 80 && matches[j].rm_so != -1; j++)
                 {
-                        int mpos = matches[j].rm_eo + 1;
-                        if (mpos > ret) {
+                        int mpos = matches[j].rm_eo;
+                        if (ret < 0 || mpos < ret) {
                                 matched = i;
                                 ret = mpos;
                         }
@@ -122,8 +166,8 @@ match(regex_t *regexps, char *buf, ssize_t pos)
         }
         if (ret >= 0)
                 ret++;
-        if (debug && matched > 0)
-                printf("Using longest match at %zd chars: %s\n", ret, escapes[matched]);
+        if (matched > 0)
+                debug("Using shortest match at %zd chars: %s", ret-1, escapes[matched]);
 
         return ret;
 }
@@ -159,6 +203,12 @@ main(int argc, char *argv[])
                         continue; // never happens
                 }
 
+                if (!strcmp(argv[i], "-d") ||
+                    !strcmp(argv[i], "--debug")) {
+                        debug_arg_ = true;
+                        continue;
+                }
+
                 if (!strcmp(argv[i], "-s") ||
                     !strcmp(argv[i], "--space-as-escape")) {
                         escape = SPC;
@@ -185,15 +235,18 @@ main(int argc, char *argv[])
                 rc = read(infd, &c, 1);
                 if (rc < 0) {
                         if (errno == EAGAIN || errno == EINTR) {
-                                if (debug)
-                                        warnx("read() == %d; trying again.", rc);
+                                debug("read() == %d; trying again.", rc);
                                 continue;
                         }
                         err(2, "Could not read from stdin");
                 } else if (rc == 0) {
-                        if (debug)
-                                warnx("%s->DONE: read() == 0", get_state_name(state));
+                        debug("%s->DONE: read() == 0", get_state_name(state));
                         state = DONE;
+                } else {
+                        if (isprint(c))
+                                debug("%s read \'%c\'", get_state_name(state), c);
+                        else
+                                debug("%s read '\\x%02hhx'", get_state_name(state), c);
                 }
 
                 switch (state) {
@@ -201,8 +254,8 @@ main(int argc, char *argv[])
                         if (c == escape) {
                                 buf[pos++] = c;
                                 buf[pos] = '\0';
-                                if (debug)
-                                        warnx("%s->NEED_MATCH: Got ESC (\\x%02hhx)", get_state_name(state), escape);
+                                debug("%s->NEED_MATCH: Got ESC (\\x%02hhx)",
+                                      get_state_name(state), escape);
                                 state = NEED_MATCH;
                         } else {
                                 fputc(c, out);
@@ -212,29 +265,62 @@ main(int argc, char *argv[])
                 case NEED_MATCH:
                         buf[pos++] = c;
                         buf[pos] = '\0';
+                        debug("new buffer:\"%s\" pos:%zd", buf, pos);
+
+                        if (c == CR || c == NL) {
+                                debug("%s->NEED_ESCAPE: Found %s.",
+                                      get_state_name(state), c == CR ? "return" : "newline");
+                                print_buf(out, buf, pos);
+                                pos = 0;
+                                buf[pos] = '\0';
+                                state = NEED_ESCAPE;
+                                continue;
+                        }
 
                         if (pos <= 1)
                                 continue;
 
                         rc = match(regexps, buf, pos);
                         if (rc < 0) {
-                                if (c == escape) {
-                                        if (debug)
-                                                warnx("%s->NEED_MATCH: Found escape when looking for a match; advancing %zd.",
-                                                      get_state_name(state), pos);
-                                        print_buf(out, buf, pos - 1, escape);
+                                if (c == escape && pos > 1) {
+                                        debug("%s->NEED_MATCH: Found escape when looking for a match; advancing %zd.",
+                                                      get_state_name(state), pos-1);
+                                        pos--;
+                                        buf[pos] = '\0';
+                                        print_buf(out, buf, pos);
+                                        debug("memset(\"%s\", '\\0', %zd)", buf, pos+1);
+                                        memset(buf, '\0', pos+1);
                                         pos = 0;
                                         buf[pos++] = c;
                                         buf[pos] = '\0';
+                                        debug("new buffer:\"%s\" pos:%zd", buf, pos);
                                         continue;
                                 }
 
-                                if (pos >= 16) {
-                                        if (debug)
-                                                warnx("%s->NEED_ESCAPE: Escape unmatched at %zd characters",
+                                if (pos >= 16 || c == CR || c == NL) {
+                                        if (c == CR || c == NL) {
+                                                debug("%s->NEED_ESCAPE: Found %s.",
+                                                      get_state_name(state),
+                                                      c == CR ? "return" : "newline");
+                                                print_buf(out, buf, pos);
+                                        } else {
+                                                debug("%s->NEED_ESCAPE: Escape unmatched at %zd characters",
                                                       get_state_name(state), pos);
-
-                                        print_buf(out, buf, pos, escape);
+                                                /*
+                                                 * Sometimes linux booting logged
+                                                 * through screen(1) winds up with:
+                                                 * \x1b[[    5.953653]
+                                                 * So get rid of \x1b[ there,
+                                                 * because it's garbage.
+                                                 */
+                                                if (pos > 1 &&
+                                                    escape == ESC &&
+                                                    buf[0] == ESC &&
+                                                    buf[1] == '[')
+                                                        print_buf(out, buf+2, pos-2);
+                                                else
+                                                        print_buf(out, buf, pos);
+                                        }
                                         pos = 0;
                                         buf[pos] = '\0';
                                         state = NEED_ESCAPE;
@@ -245,9 +331,14 @@ main(int argc, char *argv[])
                         pos -= rc;
                         if (pos > 0) {
                                 memmove(buf, buf+rc, pos);
+                                if (buf[0] == escape) {
+                                        debug("%s->NEED_MATCH: matched %d characters",
+                                              get_state_name(state), rc);
+                                        state = NEED_MATCH;
+                                }
                         } else {
-                                if (debug)
-                                        warnx("%s->NEED_ESCAPE: matched %d characters", get_state_name(state), rc);
+                                debug("%s->NEED_ESCAPE: matched %d characters",
+                                      get_state_name(state), rc);
                                 state = NEED_ESCAPE;
                         }
                         buf[pos] = '\0';
@@ -255,7 +346,7 @@ main(int argc, char *argv[])
 
                 case DONE:
                         if (pos)
-                                print_buf(out, buf, pos, escape);
+                                print_buf(out, buf, pos);
                         continue;
                 }
         }
